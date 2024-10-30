1eeb0d2d46fd27479076c7cf43c08cfc8672f103
kjhgme <kjhgme@naver.com>
2024-10-30 18:01:15 +0900

    화면 테두리 조정

--- EngineCore/Level.cpp
+++ EngineCore/Level.cpp
@@ -59,7 +59,7 @@ void ULevel::ScreenClear()
 	UEngineWinImage* BackBufferImage = MainWindow.GetBackBuffer();
 	FVector2D Size = MainWindow.GetWindowSize();
 
-	Rectangle(BackBufferImage->GetDC(), 0, 0, Size.iX(), Size.iY());
+	Rectangle(BackBufferImage->GetDC(), -2, -2, Size.iX() + 2, Size.iY() + 2);
 }
 
 void ULevel::DoubleBuffering()
