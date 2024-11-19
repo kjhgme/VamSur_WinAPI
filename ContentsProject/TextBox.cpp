bb87b7b1d4661628804703cd4d4b7199a5783768
kjhgme <kjhgme@naver.com>
2024-11-19 11:29:43 +0900

    특수 기호 추가

--- ContentsProject/TextBox.cpp
+++ ContentsProject/TextBox.cpp
@@ -123,7 +123,11 @@ int ATextBox::MapCharacterToSpriteIndex(char Character)
     //기호 매핑
     switch (Character)
     {
-    //case '!': return 63;
+    case '.': return 63;
+    case ',': return 64;
+    case '(': return 65;
+    case ')': return 66;
+    case '!': return 67;
     default: return -1; 
     }
 }
